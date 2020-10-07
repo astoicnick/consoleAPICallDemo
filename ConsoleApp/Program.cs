98e3d87230a13f62494c4dfad4aa69ce262bda6c
Nicholas Perry <nicholasperrypro@gmail.com>
2020-10-06 20:13:06 -0400

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SWAPIHelper swapiHelper = new SWAPIHelper();
+
+            string lukesName = swapiHelper.GetPersonNameByIDAsync(1).Result;
+
+            Console.WriteLine(lukesName);
+
+            Console.ReadKey();
+
+            // Get people and starships by ID
+        }
+    }
+}
---
+++ ConsoleApp/SWAPIHelper.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class SWAPIHelper
+    {
+        private const string _baseURL = "https://swapi.dev/api/";
+        private HttpClient httpClient = new HttpClient();
+        public SWAPIHelper()
+        {
+            httpClient.BaseAddress = new Uri(_baseURL);
+        }
+
+        public async Task<string> GetPersonNameByIDAsync(int id)
+        {
+            string personsName;
+
+            HttpResponseMessage nameRequest = await httpClient.GetAsync("people/" + id);
+            // https://swapi.dev/api/people/:id
+            string responseBody = await nameRequest.Content.ReadAsStringAsync();
+
+            SWAPIPerson requestedPerson = JsonConvert.DeserializeObject<SWAPIPerson>(responseBody);
+
+            personsName = requestedPerson.Name;
+
+            return personsName;
+        }
+    }
+}
